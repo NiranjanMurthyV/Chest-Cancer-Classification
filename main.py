7e8577b4d08f1f33066455cbb8c13ba14246820c
Niranjan Murthy V <niranjan.ai22@bmsce.ac.in.com>
2025-06-01 16:59:35 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import sys
-import os
-sys.path.append(os.path.join(os.getcwd(), "src"))
-
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Logger working without export command!")
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
